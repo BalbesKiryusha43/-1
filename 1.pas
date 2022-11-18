2cd96b8bb17a5fad0be3c013a5c2f7d1e65c20c4
BalbesKiryusha43 <118163485+BalbesKiryusha43@users.noreply.github.com>
2022-11-18 23:41:51 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿Program KirillP;
+Var x,a,b:real;
+y,h:real;
+Begin
+  Writeln('Введите промежуток a и b и шаг h');
+  Read(a,b,h);
+  x:=a;
+  While x<=b do 
+    Begin
+  If x<-10 then y:=power(x,3)/power(x,(0.1*x))
+  Else if  (x<-3) then y:=power(e,x)/cos(2*x)+(-x)
+  Else y:=x - 87;
+    Writeln('x = ', x:1:2, ' y = ', y:1:2);
+    x+=h;
+    End;
+End.
